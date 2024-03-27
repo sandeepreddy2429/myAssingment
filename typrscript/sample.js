12d34c985a3544d1d055d9938304072dbc9e81bc
sandeepreddy2429 <jereddysandeep24@gmail.com>
2024-03-27 13:11:29 +0530

    first commit

---
+++ typrscript/sample.js
@@ -0,0 +1,3 @@
+let num1 = 56;
+let num2 = 89;
+console.log(num1+num2);
---
+++ typrscript/script.ts
@@ -0,0 +1,4 @@
+ var subash:number = 10;
+var sandeep : number = 20;
+var res : number =  subash+sandeep
+console.log("sum is: "+res)
